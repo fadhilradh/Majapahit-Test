21272de43a1cc71f09ca259a81f7ee6789875f70
Fadhil Radhian <fadhilradhian@gmail.com>
2021-09-07 16:42:26 +0700

    [MODIFY] app title

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 const Card = ({ post }) => {
   return (
     <div
-      className=" capitalize text-white rounded-md transition duration-150 transform hover:scale-105
+      className=" capitalize text-white hover:cursor-pointer rounded-md transition duration-150 transform hover:scale-105
      shadow-lg p-5 flex flex-col justify-center items-center h-96 w-96 bg-blue-400 hover:bg-blue-500"
     >
       <p className="text-lg capitalize font-bold mb-5">{post.title}</p>
